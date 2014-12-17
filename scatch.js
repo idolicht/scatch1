1d13dccb422fe2c21a98f06bc8a93d02072c700c
idolicht <idolicht@gmail.com>
2014-12-17 20:13:28 +0200

    Update scatch.js

--- scatch.js
+++ scatch.js
@@ -1,24 +1,87 @@
 $(document).ready(function(){
-                for(var i=0;i<=15;i++){
-		  for(var j=0;j<=15;j++){
+                for(var i=0;i<=2369;i++){
+		 
 			$('.wrapper').append('<div class="container"></div>');
-                  }
+                  
 		}
 
 enableColor();
-	});
 
-//function enableColor() {
- // $('.container').hover(function () {
- //   $(this).css({
-  //    "background-color":'#dddddd'
- //  } );
-// });
-//}
+
+
+	});
+function yellow(){
+  document.getElementById("demo").innerHTML="yellow";
+  enableYellow();
+}
+
+function green(){
+  document.getElementById("demo").innerHTML="green";
+  enableGreen();
+}
+
+function blue(){
+  document.getElementById("demo").innerHTML="blue";
+  enableBlue();
+}
+
+function red(){
+  document.getElementById("demo").innerHTML="red";
+  enableRed();
+}
+
+function black(){
+  document.getElementById("demo").innerHTML="black";
+  enableBlack();
+}
+
+function myFunction(){
+
+  document.getElementById("demo").innerHTML="random";
+  enableColor();
+}
+
+
+
 
 function enableColor() {
 $('.container').hover(function() {
 			var color='#' + Math.random().toString(16).substring(2, 8);
 			$(this).css("background-color",color);
-		});
-}
+		                  });
+                       }
+
+function enableYellow() {
+$('.container').hover(function() {
+			var color='#ffff00';
+			$(this).css("background-color",color);
+		                  });
+                       }
+
+function enableBlue() {
+$('.container').hover(function() {
+			var color='blue';
+			$(this).css("background-color",color);
+		                  });
+                       }
+
+function enableRed() {
+$('.container').hover(function() {
+			var color='red';
+			$(this).css("background-color",color);
+		                  });
+                       }
+
+function enableBlack() {
+$('.container').hover(function() {
+			var color='black';
+			$(this).css("background-color",color);
+		                  });
+                       }
+
+function enableGreen() {
+$('.container').hover(function() {
+			var color='green';
+			$(this).css("background-color",color);
+		                  });
+                       }
